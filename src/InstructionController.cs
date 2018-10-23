1cfe8b9c13e91934fe5bd485ea3fa0e6428129c3
jasperwong31 <100076014@students.swinburne.edu.my>
2018-10-23 15:18:40 -0700

    first commit

---
+++ src/InstructionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using SwinGameSDK;
+static class InstructionController
+{
+	public static void Background()
+	{
+		UtilityFunctions.DrawBackground();
+		SwinGame.DrawBitmap(GameResources.GameImage("Instruction"), 213, 52);
+	}
+	public static void HandleHighScoreInput()
+	{
+		if (SwinGame.MouseClicked(MouseButton.LeftButton) || SwinGame.KeyTyped(KeyCode.vk_ESCAPE) || SwinGame.KeyTyped(KeyCode.vk_RETURN))
+		{
+			GameController.EndCurrentState();
+		}
+	}
+}
